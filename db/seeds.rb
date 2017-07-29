5d71b890d8e6bf722dfb4c8835cd9f2be8194017
Cristian Curteanu <CristianCurteanu@users.noreply.github.com>
2017-07-30 00:18:56 +0300

    Enabled faker to production: right seed config

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require "faker"
+require "faker"
 
-if Rails.env.development?
+if Rails.env.production?
   12.times do
     User.create first_name: Faker::Name.first_name,
                 last_name:  Faker::Name.last_name,
