8cf11b491d2c5a0b8151305e2f81a20011008499
Cristian Curteanu <CristianCurteanu@users.noreply.github.com>
2017-07-30 00:07:08 +0300

    Disabled faker on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "faker"
+# require "faker"
 
 if Rails.env.development?
   12.times do
